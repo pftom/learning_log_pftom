8e3558a36d2f1eb33f817b6da8c0fcdc644dd73d
黄巍 <huan1043269994@gmail.com>
2017-10-02 19:12:49 +0800

    add 404 error

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
@@ -25,7 +25,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     
     check_topic_owner(topic, request)
 
